e21ccd492845fc33eeca4d2ff922bcbcbb4c1d46
mashnello <7679625+mashnello@users.noreply.github.com>
2021-05-05 20:48:43 +0200

    feat: add basic read and write streams

--- app.js
+++ app.js
@@ -1,32 +1,30 @@
 import { Command } from 'commander/esm.mjs'
-import { pipeline } from 'stream'
 import { createReadStream, createWriteStream } from 'fs'
+import { pipeline } from 'stream'
+import { EOL } from 'os'
+
+const readStream = (path) => createReadStream(path)
+
+const writeStream = (path) =>
+  createWriteStream(path, { flags: 'a' })
+    .on('close', () => {
+      createWriteStream(path, { flags: 'a' }).write(EOL)
+    })
 
 const program = new Command()
 
 const options = program
-  .option('-s, --shift <shift>', 'a shift')
+  .option('-s, --shift <number>', 'a shift')
   .option('-i, --input <file>', 'an input file')
   .option('-o, --output <file>', 'an output file')
-  .option('-a, --action <type>', 'an action encode/decode')
-  .parse()
-  .opts()
+  .option('-a, --action <encode/decode>', 'an action encode/decode')
+  .action(({ shift, input, output, action }) => {
+    console.log(input, output)
+    pipeline(
+      readStream(input),
+      writeStream(output),
+      (err) => err && console.error('Pipeline failed', err)
+    )
+  })
 
-
-console.log(options)
-
-const readStream = () => { }
-
-const transformStream = () => { }
-
-const writeStream = () => { }
-
-
-pipeline(
-  readStream(),
-  transformStream(),
-  writeStream(),
-).then(
-  () => console.log('Pipeline succeeded'),
-  (err) => console.error('Pipeline failed', err)
-)
+program.parse(process.argv)
