aa88e059358df35aa65b69c762d699120763cb8c
mashnello <7679625+mashnello@users.noreply.github.com>
2021-05-06 08:55:08 +0200

    feat: add basic transfromer implementation

--- src/streams.js
+++ src/streams.js
@@ -1,4 +1,5 @@
 import { createReadStream, createWriteStream } from 'fs'
+import { Transform } from 'stream'
 import { EOL } from 'os'
 
 export const readStream = (path) => createReadStream(path)
@@ -9,7 +10,20 @@ export const writeStream = (path) =>
       createWriteStream(path, { flags: 'a' }).write(EOL)
     })
 
-export const transformStream = () => (args) => {
-  console.log(args)
-  return args
-}
+export const transformStream = (shift, action) => new Transform({
+  transform(chunk, encoding, callback) {
+    const transformed = chunk
+      .toString()
+      .split('')
+      .map(letter => {
+        const charCode = letter.charCodeAt(0)
+        const isAlphabeth = letter.match(/[a-z]/i)
+
+        return isAlphabeth
+          ? String.fromCharCode(charCode + parseInt(shift))
+          : letter
+      })
+      .join('')
+    callback(null, transformed);
+  }
+})
